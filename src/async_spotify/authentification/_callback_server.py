84d53b7f062c4c7aeb7334c7c2fb3cde837a3444
HuiiBuh <nhaderer1@gmail.com>
2020-02-17 14:00:13 +0100

    Added the automated spotify code request (normally has to be performed by a user)

---
+++ src/async_spotify/authentification/_callback_server.py
@@ -0,0 +1,39 @@
+"""
+This file provides a callback server for the spotify auth process. It is intended for automated testing, so use it with
+care.
+"""
+
+from aiohttp import web
+from multidict import MultiDictProxy
+
+
+async def handle_spotify_callback(request):
+    """
+    Handle the redirection make by spotify after the oauth dialog
+    :return: json
+    """
+    query_object: MultiDictProxy = request.query
+    if "code" in query_object:
+        return web.json_response({"code": query_object["code"]})
+
+    return web.json_response({"error": "Could not get the response code from spotify"})
+
+
+def _create_callback_server(port: int = 1111, callback_route: str = '/test/api/callback'):
+    """
+    Creates a server for the callback after the oauth dialogue
+    :param port: The port the server runs on
+    :return: None
+    """
+
+    routes: list = [web.get(callback_route, handle_spotify_callback)]
+
+    app = web.Application()
+
+    app.add_routes(routes)
+    try:
+        web.run_app(app, port=port)
+    except Exception as e:
+        # Ignore the exception. Running a new Process is a async loop is not a good idea, but the only way to
+        # do it in this case
+        pass
--- src/conftest.py
+++ src/conftest.py
@@ -1,1 +1,29 @@
 import pytest
+
+from async_spotify import Preferences, API
+
+scopes = ["ugc-image-upload", "user-read-playback-state", "user-read-email", "playlist-read-collaborative",
+          "user-modify-playback-state", "user-read-private", "playlist-modify-public", "user-library-modify",
+          "user-top-read", "user-read-currently-playing", "playlist-read-private", "user-follow-read",
+          "app-remote-control",
+          "user-read-recently-played", "playlist-modify-private", "user-follow-modify", "user-library-read"]
+
+
+@pytest.fixture(scope='session')
+def preferences():
+    pref = Preferences()
+    pref.load_from_env()
+    if not pref.validate():
+        raise ValueError("Preferences are not correctly filled in")
+    return pref
+
+
+@pytest.fixture(scope='session')
+def api():
+    # Get the preferences
+    pref = Preferences()
+    pref.load_from_env()
+
+    # Create the api
+    api = API(pref)
+    return api
