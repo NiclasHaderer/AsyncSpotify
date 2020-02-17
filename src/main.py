c1305892fdb3f62d6807ecf41228043d6f7bf486
Niclas Haderer <niclas.haderer.dev@gmail.com>
2020-02-17 20:52:48 +0100

    Improved the automated code retrieval

--- src/main.py
+++ src/main.py
@@ -1,18 +1,8 @@
 import asyncio
-import json
-import subprocess
 from asyncio import sleep
-from json import JSONDecodeError
-from subprocess import CompletedProcess
 
 from async_spotify.api import API
 from async_spotify.authentification.preferences import Preferences
-
-
-class dotdict(dict):
-    __getattr__ = dict.get
-    __setattr__ = dict.__setitem__
-    __delattr__ = dict.__delitem__
 
 
 async def main():
@@ -22,7 +12,7 @@ async def main():
     preferences.load_from_docker_secret()
 
     spotify_api: API = API(preferences)
-    spotify_code = spotify_api.get_code_with_cookie("/home/niclas/Downloads/cookies.txt")
+    spotify_code = spotify_api._get_code_with_cookie("/home/niclas/IdeaProjects/AsyncSpotify/src/private/cookies.txt")
     print(spotify_code)
 
 asyncio.run(main(), debug=True)
