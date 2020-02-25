618ef05c34ae6d12f3e316863763a37a97e68e00
Niclas Haderer <niclas.haderer.dev@gmail.com>
2020-02-25 22:07:05 +0100

    Updated documentation

--- src/async_spotify/api/endpoints/albums.py
+++ src/async_spotify/api/endpoints/albums.py
@@ -5,7 +5,6 @@ Handle the requests to the albums endpoint
 from typing import List
 
 import async_spotify
-from ..decorators import get_url
 
 
 class Albums:
@@ -16,36 +15,57 @@ class Albums:
     def __init__(self, api_object):
         """
         Create a new spotify album query class which handles the queries concerning albums
-        :param api_object: The api object the class is assigned to
+
+        Args:
+            api_object: The api object the class is assigned to
         """
 
         self.api_object = api_object  # type: async_spotify.API
 
-    @get_url
     async def get_album(self, album_id: str, **kwargs) -> dict:
         """
         Get the album with the specific spotify album id
-        https://developer.spotify.com/documentation/web-api/reference/albums/get-album/
-        :param album_id: The album id of the album you want to get
-        :param kwargs: Optional arguments as keyword args
-        :return: The album json
+
+        Args:
+            album_id: The album id of the album you want to get
+            kwargs: Optional arguments as keyword args
+
+        Note:
+            [https://developer.spotify.com/documentation/web-api/reference/albums/get-album/](https://developer.spotify.com/documentation/web-api/reference/albums/get-album/)
+
+        Returns:
+            The album json
         """
 
         required_args = {"id": album_id}
         return {**required_args, **kwargs}
 
-    async def get_album_tracks(self, album_id: str):
+    async def get_album_tracks(self, album_id: str) -> dict:
         """
+        Get the tracks of an album
 
-        :param album_id:
-        :return:
+        Args:
+            album_id: The id of the album
+
+        Note:
+            [https://developer.spotify.com/documentation/web-api/reference/albums/get-albums-tracks/](https://developer.spotify.com/documentation/web-api/reference/albums/get-albums-tracks/)
+
+        Returns:
+            The api response from spotify
         """
         pass
 
-    async def get_multiple_albums(self, id_list: List[str], market: str = None):
+    async def get_multiple_albums(self, album_id_list: List[str]) -> dict:
         """
+        Get All the albums specified in the album_id_list
 
-        :param id_list:
-        :param market:
+        Args:
+            album_id_list: The list of the spotify album ids
+
+        Note:
+            [https://developer.spotify.com/documentation/web-api/reference/albums/get-several-albums/](https://developer.spotify.com/documentation/web-api/reference/albums/get-several-albums/)
+
+        Returns:
+            The api response from spotify
         """
         pass
--- src/async_spotify/api/endpoints/artists.py
+++ src/async_spotify/api/endpoints/artists.py
@@ -12,7 +12,9 @@ class Artist:
     def __init__(self, api_object):
         """
         Create a new spotify artist query class which handles the queries concerning artists
-        :param api_object: The api object the class is assigned to
+
+        Args:
+            api_object: The api object the class is assigned to
         """
 
         self.api_object = api_object  # type: async_spotify.API
@@ -20,8 +22,15 @@ class Artist:
     def get_artist(self, artist_id: str):
         """
         Get the artist by id
-        :param artist_id: The artist id
-        :return: The json associated with the artist
+
+        Args:
+            artist_id: The artist id
+
+        Note:
+            [https://developer.spotify.com/documentation/web-api/reference/artists/get-artist/](https://developer.spotify.com/documentation/web-api/reference/artists/get-artist/)
+
+        Returns:
+            The json associated with the artist
         """
 
         pass
