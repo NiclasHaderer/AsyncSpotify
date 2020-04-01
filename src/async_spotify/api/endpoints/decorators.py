2d18c352840814dc4ee1c3d09a8e33187032f9fa
Niclas Haderer <niclas.haderer.dev@gmail.com>
2020-04-01 15:53:53 +0200

    Added other class files Completed the urls Finished the decorator

--- src/async_spotify/api/endpoints/decorators.py
+++ src/async_spotify/api/endpoints/decorators.py
@@ -2,11 +2,11 @@
 A file with a wrapper functions
 """
 from functools import wraps
+from typing import Tuple
 
+from .objects import DecoratorInformationObject
 from ..api_request_maker import ApiRequestHandler
 from ...authentification.spotify_authorization_token import SpotifyAuthorisationToken
-
-api_request_handler = ApiRequestHandler()
 
 
 def make_request(url: str, method: str):
@@ -14,14 +14,14 @@ def make_request(url: str, method: str):
     Wrap a get function
 
     Args:
-        method: The method that should be used to make the request
         url: The url that should be called
+        method: The method that should be used to make the request
 
     Returns:
         The executable function
     """
 
-    def outer_wrapper(function):
+    def outer_wrapper(function: callable) -> callable:
         """
         Return the wrapper function so the wrapper function can be called
 
@@ -33,7 +33,7 @@ def make_request(url: str, method: str):
         """
 
         @wraps(function)
-        async def wrapper(*args, **kwargs):
+        async def wrapper(*args, **kwargs) -> dict:
             """
             The wrapper that wraps the function to get the function arguments
 
@@ -46,20 +46,40 @@ def make_request(url: str, method: str):
             """
 
             # Get the args from the original api function
-            return_values = function(*args, **kwargs)[0]
-            query_params = return_values[0]
-            auth_token: SpotifyAuthorisationToken = return_values[1]
+            return_values: DecoratorInformationObject = await function(*args, **kwargs)
 
-            if method == "GET":
-                response: dict = await api_request_handler.get_request(url, query_params, auth_token)
+            # Get the params required for making the api call
+            query_params: dict = return_values.arguments
+            auth_token: SpotifyAuthorisationToken = return_values.auth_token
+            api_request_handler: ApiRequestHandler = return_values.request_maker
 
-            if method == "PUT":
-                response: dict = await api_request_handler.put_request(url, query_params, auth_token)
+            # Fill in the required url params
+            updated_url, query_params = format_map(url, query_params)
 
-            return response
+            # Make the api request
+            return await api_request_handler.make_request(method, updated_url, query_params, auth_token)
 
         return wrapper
 
     return outer_wrapper
 
-# TODO save auth if token expired
+
+def format_map(string: str, map_object: dict) -> Tuple[str, dict]:
+    """
+    Formats a string with the map keys and returns the formatted string and the map without the used keys
+
+    Args:
+        string: The string which should be formatted
+        map_object: The map which should be used form formatting
+
+    Returns:
+        Tuple(the formatted string, the dict without the used values)s
+
+    """
+
+    for key in list(map_object.keys()):
+        if '{' + key + '}' in string:
+            string = string.format_map({f'{key}': map_object[key]})
+            map_object.pop(key, None)
+
+    return string, map_object
